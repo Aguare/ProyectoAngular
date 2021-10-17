f49cb8a07b1fa5ba03491d3d48e77621d542d1fc
Aguare <marcosy300@gmail.com>
2021-10-16 21:34:47 -0600

    Me tenía que cambiar de rama

--- App-Revistas/src/app/Componentes/Revistas/publicar-revista/publicar-revista.component.ts
+++ App-Revistas/src/app/Componentes/Revistas/publicar-revista/publicar-revista.component.ts
@@ -8,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class PublicarRevistaComponent implements OnInit {
 
   crearEtiqueta: boolean = true;
+  esGratuita: boolean = false;
+  archivo: File | null = null;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  cambiarEsGratuita() {
+    let input = document.getElementById("inputPrecioSuscripcion");
+    if (this.esGratuita) {
+      this.esGratuita = false;
+      input?.removeAttribute("disabled");
+      input?.setAttribute("required", "");
+    } else {
+      this.esGratuita = true;
+      input?.removeAttribute("required");
+      input?.setAttribute("disabled", "");
+    }
+  }
+
+  obtenerPath(event: Event) {
+    const files = (event.target as HTMLInputElement).files;
+    let input = document.getElementById("inputFile");
+    if (files != null) {
+      this.archivo = files.item(0);
+    }
+  }
 }
